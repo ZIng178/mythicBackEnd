360b82766925fe8037af5bc36c9ec03f4c56e04d
Wangdi Tenzing <wangdi457@gmail.com>
2021-10-18 12:12:04 -0400

    wrote end points for GET and DELETE methods GET route uses the access token from login and checks if the user is an admin The route  only authorizes admin to get all the users GET Works good on Postman

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,12 @@
 const router = require("express").Router();
-const { verifyToken } = require("./verifyToken");
+const {
+  verifyToken,
+  verifyTokenAuthorization,
+  verifyTokenAdmin,
+} = require("./verifyToken");
 const CryptoJS = require("crypto-js");
 const User = require("../models/User");
+const jwt = require("jsonwebtoken");
 
 //UPDATE
 router.put("/:id", verifyToken, async (req, res) => {
@@ -20,6 +25,28 @@ router.put("/:id", verifyToken, async (req, res) => {
       { new: true }
     );
     res.status(200).json(updatedUser);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//DELETE
+
+router.delete("/:id", verifyTokenAuthorization, async (req, res) => {
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.status(200).json("User has benn deleted");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//GET USER
+router.get("/find/:id", verifyTokenAdmin, async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    const { password, ...others } = user._doc;
+    res.status(200).json(others);
   } catch (err) {
     res.status(500).json(err);
   }
--- routes/verifyToken.js
+++ routes/verifyToken.js
@@ -4,7 +4,7 @@ const verifyToken = (req, res, next) => {
   const authHeader = req.headers.token;
   if (authHeader) {
     const token = authHeader.split(" ")[1];
-    console.log(token);
+
     jwt.verify(token, process.env.JWT_SEC, (err, user) => {
       if (err) res.status(403).json("Token is not valid");
       req.user = user;
@@ -15,7 +15,7 @@ const verifyToken = (req, res, next) => {
   }
 };
 
-const veryifyTokenAuthorization = (req, res, next) => {
+const verifyTokenAuthorization = (req, res, next) => {
   verifyToken(req, res, () => {
     if (req.user.id === req.params.id || req.user.isAdmin) {
       next();
@@ -25,4 +25,13 @@ const veryifyTokenAuthorization = (req, res, next) => {
   });
 };
 
-module.exports = { verifyToken };
+const verifyTokenAdmin = (req, res, next) => {
+  verifyToken(req, res, () => {
+    if (req.user.isAdmin) {
+      next();
+    } else {
+      res.status(403).json("You are not authorized to make changes ");
+    }
+  });
+};
+module.exports = { verifyToken, verifyTokenAuthorization, verifyTokenAdmin };
