56cedac588813e759febc150128714f2b485a786
Wangdi Tenzing <wangdi457@gmail.com>
2021-10-21 13:12:33 -0400

    added stripe added a stripe route

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const authRoute = require("./routes/auth");
 const productRoute = require("./routes/product");
 const cartRoute = require("./routes/cart");
 const orderRoute = require("./routes/order");
+const stripeRoute = require("./routes/stripe");
+const cors = require("cors");
 
 dotenv.config();
 
@@ -16,12 +18,14 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+app.use(cors());
 app.use(express.json());
 app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
 app.use("/api/Products", productRoute);
 app.use("/api/carts", cartRoute);
 app.use("/api/orders", orderRoute);
+app.use("/api/checkout", stripeRoute);
 
 app.listen(5001, () => {
   console.log("Backend server is running ");
